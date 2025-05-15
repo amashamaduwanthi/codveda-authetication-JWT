0eb3c534498203fbae1b3d1d974cfdfacea03a2f
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-05-15 20:51:01 +0530

    mongoose schema create

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import express from 'express';
 import mongoose from 'mongoose';
+import express from 'express';
 const app = express();
 app.use(express.json())
 mongoose.connect("mongodb://localhost:27017/Authentication")
@@ -9,6 +9,3 @@ mongoose.connect("mongodb://localhost:27017/Authentication")
 app.use('/',(req,res,next)=>{
     res.status(200).send('Not Found');
 })
-
-
-
